2ff5d7d6d693b14ae486c7ddcf602fb0c9198dfd
Freckleface Cogswell <jeffcogs@gmail.com>
2026-03-29 12:50:10 -0600

    feat: add a flake for nix/npm

---
+++ typescript/nodejs_and_npm/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.npm
+          ];
+
+          shellHook = ''
+            echo "Node $(node --version) / npm $(npm --version)"
+          '';
+        };
+      }
+    );
+}
